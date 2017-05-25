6d907ae56a20de07cf09b1cb8a472d447b585b79
wg <247026287@qq.com>
2017-05-25 19:38:59 +0800

    animationName add random

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsPubRoot: path.resolve(__dirname, '../pub'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
